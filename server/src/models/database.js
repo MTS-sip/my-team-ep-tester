15c7660355b8bbd224795620ee4f053ac88b0c7b
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-02-25 18:15:53 -0500

    env

--- server/src/models/database.js
+++ server/src/models/database.js
@@ -4,17 +4,21 @@ import dotenv from "dotenv";
 // Load environment variables
 dotenv.config();
 
-console.log (process.env); 
+console.log(process.env);
 // Validate required environment variables
-if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_HOST) {
-  throw new Error("❌ Missing required database environment variables in .env file.");
-}
 
 // Initialize Sequelize instance
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "postgres",
-  logging: false, // Disable logging (optional)
-});
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME || "",
+      process.env.DB_USER || "",
+      process.env.DB_PASSWORD || "",
+      {
+        host: process.env.DB_HOST || "localhost",
+        dialect: "postgres",
+        logging: false, // Disable logging (optional)
+      }
+    );
 
 export default sequelize;
