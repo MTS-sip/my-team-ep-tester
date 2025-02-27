11f2da210db1f13a4841dcc7720069d7ed7e3e89
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-02-27 09:52:43 -0500

    persist again

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,52 +1,20 @@
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
-import "./index.css";
-
-import App from "./App.tsx";
-import ErrorPage from "./pages/ErrorPage.tsx";
-import Home from "./pages/Home.tsx";
-import Login from "./pages/Login.tsx";
-import Create from "./pages/Create.tsx";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/create-event" element={<Create />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Route>
-  )
-);
-
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
-}
-
-
-/*
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
 import App from './App.tsx';
 
-import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 import Create from './pages/Create.tsx';
-
-
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />,
     children: [
       {
-        index: true,
+        path: '/Home',
         element: <Home />,
       },
       {
@@ -64,6 +32,7 @@ const router = createBrowserRouter([
 const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
-}   
-  
-*/ 
+}
+
+
+
