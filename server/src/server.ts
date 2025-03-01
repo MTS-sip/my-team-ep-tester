9891d458f06321cbeec2e1ec8177fa5262ef1240
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-03-01 02:16:16 -0500

    back track

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,4 @@
 import express from "express";
-
-import { sequelize } from "./config/connections.js";
-import authRoutes from "./routes/auth.js";
-import eventRoutes from "./routes/events.js";
-import { fileURLToPath } from "url"; //  define `__dirname`
-import { dirname } from "path";
 import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -19,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ Enable CORS
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://my-team-ep-tester.onrender.com"], // ✅ Allow frontend origin
+    origin: ["http://localhost:3000", "https://event-planner-anuy.onrender.com"], // ✅ Allow frontend origin
     credentials: true, // ✅ Allow cookies/auth headers
   })
 );
@@ -42,36 +36,14 @@ import eventRoutes from "./routes/events.js";
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 
-app.use(express.json());
-
-// Database connection
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Database connected successfully");
-    await sequelize.sync();
-  } catch (error) {
-    console.error("❌ Database connection failed:", error);
-   }
- };
-connectDB();
-
-// Root endpoint
-app.get("*", (_, res) => {  // *asteraisk for all, rcompare slash /
- res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
-  //res.send("🎉 Welcome to Occasionally, your place for Event Planning, Online, Organized, for your Most Memorable and Greatest Gatherings. OMG²!");
-
 // ✅ Serve React frontend (catch-all for non-API routes)
 app.get("*", (_, res) => {
   res.sendFile(path.join(clientBuildPath, "index.html"));
-
 });
 
 // ✅ Start Server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
-
-
 
 
