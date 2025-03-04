1d88154e18f4dde3febcdf577e4a69488260fb0a
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-03-04 14:43:22 -0500

    event vs events

--- server/src/routes/event.js
+++ server/src/routes/event.js
@@ -1,6 +1,6 @@
 // chs
 import express from "express";
-import Event from "../models/event";
+import Event from "../models/event.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
